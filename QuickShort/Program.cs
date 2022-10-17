fff7c3a153c833cf3aadf438e3f145ed3c114975
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-10-17 20:22:40 +0700

    Mengganti program yang salah di line 59 dan 70

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -1,67 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography;
+using System.Xml;
 
-namespace QuickShort
+namespace QuickSort
 {
     class Program
     {
         //array of integers to hold values
         private int[] arr = new int[20];
-        private int cmp_count = 0; // number of comparasion
-        private int mov_count = 0; // number of data movements
+        private int cmp_count = 0;
+        private int mov_count = 0;
 
-        //number of elements in array 
+        //Number of elements in array
         private int n;
-
 
         void read()
         {
             while (true)
             {
-                Console.Write("Enter the number of elements in the array :");
+                Console.WriteLine("Enter the number of elements in the array :");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
                 if (n <= 20)
                     break;
                 else
-                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+                    Console.WriteLine("\n Array can have maximum 20 elements \n");
             }
-            Console.WriteLine("\n=================================");
+            Console.WriteLine("\n====================");
             Console.WriteLine("Enter Array Elements");
-            Console.WriteLine("\n=================================");
+            Console.WriteLine("\n====================");
 
             //get array elements
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
-                Console.Write("<" + (i+1) + ">");
-                string s1 =Console.ReadLine();
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
             }
-        }
 
+        }
         //swaps the element at index x with the element at index y
         void swap(int x, int y)
         {
             int temp;
-
             temp = arr[x];
             arr[x] = arr[y];
             arr[y] = temp;
         }
-        public void q_short(int low, int high)
+
+        public void q_sort(int low, int high)
         {
             int pivot, i, j;
             if (low > high)
                 return;
 
-            //partition the list into two parts:
-            //one containing elements less that or equal tompivot
-            //other conntainning elemnts greather than pivot
+            //Partition the list into two parts
+            //One containing elements less that or equal to pivot
+            //Outher containing elements greater than pivot
 
-            i = low + i;
+            i = low + 1;
             j = high;
 
             pivot = arr[low];
@@ -69,68 +67,75 @@ namespace QuickShort
             while (i <= j)
             {
                 //search for an element greater than pivot
-                while ((arr[i] <= pivot) && (j >= low))
+                while ((arr[i]) <= pivot && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                //search for an element less than or equal to pivot
+                while ((arr[j]) > pivot && (j >= low))
                 {
                     j--;
                     cmp_count++;
                 }
                 cmp_count++;
-                if (i<j) //if the grater element is on the left of the element
+
+                if (i < j) //if the greater element is on the left of the element
                 {
-                    //swap the element at index i whit the element at index j
+                    //swap the element at index i with the element at index j
                     swap(i, j);
                     mov_count++;
                 }
             }
+            // j now contains the index of the last elements int the sorted list
 
-            //j now contains the index of the last element in the sorted list
-
-            if (low<j)
+            if (low < j)
             {
-                //move the pivot to its correct positon in the list
-                swap(low,j);
+                //Move the pivot to its correct position in the list
+                swap(low, j);
                 mov_count++;
             }
-            //short the list on the left of pivot using quick short
-            q_short(low, j - 1);
+            //sort the list on the left of pivot using quick sort
+            q_sort(low, j - 1);
 
-            //short the list on the right of pivot using quick short
-            q_short(j+1, high);
-
-            
+            //sort the list on the right of pivot using quick sort
+            q_sort(j + 1, high);
         }
 
         void display()
         {
-            Console.WriteLine("\n====================================");
-                Console.WriteLine("Sorted array elements");
-            Console.WriteLine("======================================");
+            Console.WriteLine("\n-------------------");
+            Console.WriteLine("Sorted Array Elements");
+            Console.WriteLine("\n-------------------");
 
-            for(int j = 0; j < n; j++)
+            for (int j = 0; j < n; j++)
             {
                 Console.WriteLine(arr[j]);
             }
-            Console.WriteLine("\nNumber of comparisons: "+ cmp_count);
-            Console.WriteLine("\nNumber of data movements: "+ mov_count);
+            Console.WriteLine("\n Number of Comparison: " + cmp_count);
+            Console.WriteLine("\n Number of Data Movements: " + mov_count);
         }
 
         int getSize()
         {
             return n;
         }
+
         static void Main(string[] args)
         {
             //Declaring the object of the class
             Program mylist = new Program();
-            //Accept array elements
+            //Accept Array Elements
             mylist.read();
-            //calling the sorting function
-            //first call to quick short Alogarithm
-            mylist.q_short(0, mylist.getSize() - 1);
-            //display sorted array
+            //Calling the sorting function
+            //First call to quick sort Alogarithm
+            mylist.q_sort(0, mylist.getSize() - 1);
+            //Display sorted array
             mylist.display();
             //to exit from the console
-            Console.WriteLine("\n\nPress Enter to exit.");
+            Console.WriteLine("\n\n Press enter to exit.");
             Console.Read();
         }
     }
